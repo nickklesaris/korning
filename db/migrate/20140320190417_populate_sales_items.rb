0442034fbcb57797b5077d1f55031e7bc53f7388
Nick Klesaris <nickklesaris@gmail.com>
2014-03-20 16:58:41 -0400

    populate sales items union table

--- db/migrate/20140320190417_populate_sales_items.rb
+++ db/migrate/20140320190417_populate_sales_items.rb
@@ -2,11 +2,11 @@ class PopulateSalesItems < ActiveRecord::Migration
   def change
 
     Sale.find_each do |sale|
-      SaleItem.find_or_create_by(sale_id: sale.id) do |product|
+      SaleItem.find_or_create_by(sale_id: sale.id) do |sale_item| # Be careful not to clash iteration_names when nesting do's
         sale_item.product_id = sale.product_id
-        sale_item.unit_sold = sale.unit_sold
+        sale_item.unit_sold = sale.units_sold
         sale_item.unit_price = (sale.sale_amount/sale.units_sold)
-        sale_item.save
+        #sale_item.save
       end
     end
   end
